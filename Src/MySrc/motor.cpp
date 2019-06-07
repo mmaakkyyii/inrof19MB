6f0901b9e589a42f179a1a61a01a9bf75773c7e6
mmaakkyyii <kyoro3141592@gmail.com>
2019-06-08 00:50:23 +0900

    SPI通信まわり変更

--- Src/MySrc/motor.cpp
+++ Src/MySrc/motor.cpp
@@ -13,7 +13,7 @@ void Motor::Drive(float v){
 	uint8_t rietion[3];
 	rietion[0]=((int16_t)v) >> 8;
 	rietion[1]=((int16_t)v) & 0xff;
-	rietion[2]=0xff;
+	rietion[2]=rietion[0]^rietion[1];
 
 	HAL_GPIO_WritePin(port,pin,GPIO_PIN_RESET);
 	HAL_SPI_Transmit(&hspi1,(uint8_t*)rietion,3,200);
